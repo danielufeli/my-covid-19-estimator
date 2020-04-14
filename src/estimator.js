fcb4e76cb2f3cbea28d33daaa459d1bc91e9aaf9
Daniel Ufeli <danielufeli@yahoo.com>
2020-04-14 22:51:17 +0100

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,25 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = ({ reportedCases }) => ({
+  data: {
+    region: {
+      name: 'Africa',
+      avgAge: 19.7,
+      avgDailyIncomeInUSD: 5,
+      avgDailyIncomePopulation: 0.71
+    },
+    periodType: 'days',
+    timeToElapse: 58,
+    reportedCases,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+  },
+  impact: {
+    currentlyInfected: reportedCases * 10,
+    infectionsByRequestedTime: this.currentlyInfected * 512
+  },
+  severeImpact: {
+    currentlyInfected: reportedCases * 50,
+    infectionsByRequestedTime: this.currentlyInfected * 512
+  }
+});
 
 export default covid19ImpactEstimator;
