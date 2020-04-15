2f836c56ec8c3d2c36be1cba3f703838bd9b3f5d
Daniel Ufeli <danielufeli@yahoo.com>
2020-04-15 01:30:57 +0100

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,12 +1,25 @@
 const covid19ImpactEstimator = (data) => {
   const impact = {};
   const severeImpact = {};
-  const days = 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2;
+  const time = (period) => {
+    let m = 0;
+    if (period === 'days') {
+      m = 2 ** Number(data.timeToElapse % 3);
+    }
+    if (period === 'week') {
+      m = 2 ** Number((data.timeToElapse * 7) % 3);
+    }
+    if (period === 'month') {
+      m = 2 ** Number((data.timeToElapse * 30) % 3);
+    }
+    return m;
+  };
+  const timeE = time(data.periodType);
   impact.currentlyInfected = data.reportedCases * 10;
-  impact.infectionsByRequestedTime = impact.currentlyInfected * days;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * timeE;
 
   severeImpact.currentlyInfected = data.reportedCases * 50;
-  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * days;
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * timeE;
   return {
     data,
     impact,
